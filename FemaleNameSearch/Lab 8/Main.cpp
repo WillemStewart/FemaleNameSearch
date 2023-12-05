5e0b4fe64c0c8b14124846812761eda9f41c9568
Willem Stewart <100379801+WillemStewart@users.noreply.github.com>
2023-12-05 14:59:01 -0600

    Add files via upload

---
+++ FemaleNameSearch/Lab 8/Main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <cctype>
+#include "Name.h"
+using namespace std;
+
+int main()
+{
+	ifstream nameFile;
+	string name;
+	string newName, nameCounter;
+	int newNameSize;
+	int ord;
+	Name nameList;
+
+	nameFile.open("CommonFemaleNames.txt");
+	while (nameFile >> ord)
+	{
+		nameFile >> name;
+		nameList.addName(new CommonName(ord, name));
+	}
+	
+	cout << "The names have been read" << endl;
+
+	nameList.sortNames();
+
+	cout << "Enter a female name, enter stop to end" << endl;
+	cin >> newName;
+
+	while (newName != "stop")
+	{
+		nameCounter = "";
+		newNameSize = newName.length();
+		for (int i = 0; i < newNameSize; i++)
+		{
+			nameCounter += toupper(newName.at(i));
+		}
+		newName = nameCounter;
+
+		auto start_time = chrono::high_resolution_clock::now();
+		ord = nameList.findLinear(newName);
+		auto end_time = chrono::high_resolution_clock::now();
+		cout << "Linear search took " << chrono::duration_cast<chrono::microseconds>(end_time - start_time).count() << " microseconds." << endl;
+
+		start_time = chrono::high_resolution_clock::now();
+		nameList.findBinary(newName);
+		end_time = chrono::high_resolution_clock::now();
+		cout << "Binary search took " << chrono::duration_cast<chrono::microseconds>(end_time - start_time).count() << " microseconds." << endl;
+		
+		if (ord <= 0 || ord == 69)
+		{
+			cout << newName << " is not on the list" << endl;
+		}
+		else
+		{
+			cout << newName << " is the number " << ord << " most popular female name" << endl;
+		}
+
+		cout << "Enter a female name, enter stop to end" << endl;
+		cin >> newName;
+	}
+	return 0;
+}
